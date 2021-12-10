13124ce53245d9668465aac201462f38bcfc619b
ZENBOOK\aurum <aurum1997@gmail.com>
2021-12-10 15:47:31 +0300

    Незначительные изменения

--- homework2/Controllers/WeatherController.cs
+++ homework2/Controllers/WeatherController.cs
@@ -5,24 +5,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace homework2.Controllers
 {
-	[Route("weather")]
-	public class WeatherController : Controller
-	{
-		private readonly OpenWeatherClient _openWeatherClient;
+    [Route("weather")]
+    public class WeatherController : Controller
+    {
+        private readonly OpenWeatherClient _openWeatherClient;
 
-		public WeatherController(OpenWeatherClient openWeatherClient)
-		{
-			_openWeatherClient = openWeatherClient;
-		}
-
-		public async Task<ActionResult> Get([Required, FromQuery(Name = "city")]string cityName)
-		{
-			if (!ModelState.IsValid)
-				return BadRequest("Name of a city is not provided");
-
-			var currentWeatherDto = await _openWeatherClient.GetWeatherAsync(cityName);
-
-			return Ok(currentWeatherDto);
-		}
-	}
+        public WeatherController(OpenWeatherClient openWeatherClient)
+        {
+            _openWeatherClient = openWeatherClient;
+        }
+		
+        public async Task<ActionResult> Get([Required, FromQuery(Name = "city")]string cityName)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Name of a city is not provided");
+		
+            var currentWeatherDto = await _openWeatherClient.GetWeatherAsync(cityName);
+		
+            return Ok(currentWeatherDto);
+        }
+    }
 }
