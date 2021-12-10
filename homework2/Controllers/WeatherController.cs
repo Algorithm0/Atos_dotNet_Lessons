99199e8a70d6daf6b75142056837555a4103e09e
ZENBOOK\aurum <aurum1997@gmail.com>
2021-12-10 12:21:22 +0300

    Копирование изначального кода для домашнего задания.

---
+++ homework2/Controllers/WeatherController.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using homework2.OpenWeather;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework2.Controllers
+{
+	[Route("weather")]
+	public class WeatherController : Controller
+	{
+		private readonly OpenWeatherClient _openWeatherClient;
+
+		public WeatherController(OpenWeatherClient openWeatherClient)
+		{
+			_openWeatherClient = openWeatherClient;
+		}
+
+		public async Task<ActionResult> Get([Required, FromQuery(Name = "city")]string cityName)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest("Name of a city is not provided");
+
+			var currentWeatherDto = await _openWeatherClient.GetWeatherAsync(cityName);
+
+			return Ok(currentWeatherDto);
+		}
+	}
+}
