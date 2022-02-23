9ad47a4a041e69ac48cad26da93fe1a66c35edf3
ZENBOOK\aurum <aurum1997@gmail.com>
2022-02-23 17:29:11 +0300

    Init homework3

---
+++ homework3/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using homework3.DataAccess;
+using homework3.DataAccess.Entity;
+
+namespace homework3.Controllers
+{
+	[Route("user")]
+	public class UserController : Controller
+	{
+		private readonly BookingContext _bookingContext;
+
+		public UserController(BookingContext bookingContext)
+		{
+			_bookingContext = bookingContext;
+		}
+
+		public async Task<ActionResult<string[]>> GetAllUserNames()
+		{
+			return await _bookingContext.Users
+				.Select(u => u.UserName)
+				.ToArrayAsync();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<User>> CreateUser([FromBody]User user)
+		{
+			User userInDb = await _bookingContext
+				.Users
+				.FirstOrDefaultAsync(u => u.UserName == user.UserName);
+
+			if (userInDb != null)
+				return Conflict("User with this name already exists");
+
+			_bookingContext.Users.Add(user);
+			await _bookingContext.SaveChangesAsync();
+
+			return Ok(user);
+		}
+	}
+}
